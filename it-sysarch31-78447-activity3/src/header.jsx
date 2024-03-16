55233b903144079428435cd58ff10cc18e036bf5
James Anthony Ceniza <jamesniza1@gmail.com>
2024-03-16 09:50:58 +0800

    First Commit

---
+++ it-sysarch31-78447-activity3/src/header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+
+    return(
+      <header>
+        
+        <nav>
+            <div className="navbar">
+              <h1>POKEDEX</h1>
+            </div>  
+          </nav>
+        <hr></hr>
+      </header>
+    );
+    
+  }
+  
+  export default Header 
---
+++ it-sysarch31-78447-activity3/src/pokedex.jsx
@@ -0,0 +1,12 @@
+function Pokedex() {
+    return (
+      <div>
+        <button className="button">English</button>
+        <button className="button">Japanese</button>
+        <button className="button">Chinese</button>
+        <button className="button">French</button>
+      </div>
+    );
+  }
+  
+  export default Pokedex;
